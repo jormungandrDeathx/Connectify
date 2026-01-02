f37eda54bc55194be737b61c67ba0e026891c1d3
jormungandrDeathx <jormungandr.deathx@gmail.com>
2026-01-02 17:06:29 +0530

    made changes in routing

--- backend/api/routing.py
+++ backend/api/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 from .consumers import GlobalConsumer, ChatConsumer, FriendRequestsNotification,PresenceConsumer
 
-websocket_urlpattern = [
+websocket_urlpatterns = [
     re_path(r"^ws/chat/$",ChatConsumer.as_asgi()),
     re_path(r"^ws/chat/global/$",GlobalConsumer.as_asgi()),
     re_path(r"^ws/friendRequestNotification/$",FriendRequestsNotification.as_asgi()),
