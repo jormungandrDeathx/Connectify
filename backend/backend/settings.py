6f521c7a188931a7563bcdc59f28b01dd9fd1cf8
jormungandrDeathx <jormungandr.deathx@gmail.com>
2026-01-05 18:29:45 +0530

    changes made in backend[Email]

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,13 +93,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 ASGI_APPLICATION = 'backend.asgi.application'
 
 
-EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST=os.getenv("EMAIL_HOST")
-EMAIL_PORT=os.getenv("EMAIL_PORT")
-EMAIL_USE_TLS=True
-EMAIL_USE_SSL=False
-EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
+
 
 
 CHANNEL_LAYERS={
