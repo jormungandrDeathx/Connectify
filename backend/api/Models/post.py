149a02f2ce97ecae9e0a5c3ec311382087349a94
jormungandrDeathx <jormungandr.deathx@gmail.com>
2026-01-07 15:44:44 +0530

    changes in fieldlength

--- backend/api/Models/post.py
+++ backend/api/Models/post.py
@@ -5,7 +5,7 @@ class Posts(db.Model):
     userName = db.CharField( max_length=100)
     userImage = db.ImageField(upload_to="Media/Posts/userImages/",default="Avatar_hzjeqe.jpg", max_length=500 , blank=True, null=True)
     message = db.TextField()
-    postImage = db.ImageField(upload_to="Media/Posts/userPosts/",blank=True,null=True)
+    postImage = db.ImageField(upload_to="Media/Posts/userPosts/",max_length=500 ,blank=True,null=True)
     likes = db.PositiveIntegerField(default=0)
     createdAt = db.DateTimeField()
     
